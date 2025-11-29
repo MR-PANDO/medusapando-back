6fd306893b0189ec80fc57ad08f39b018c5ecfaa
MR-PANDO <gerencia@tecnoclinica.com>
2025-11-29 00:50:55 -0500

    colombia cities module

--- migrations.sh
+++ migrations.sh
@@ -3,6 +3,12 @@
 if [ "$WORKER_MODE" != "worker" ]; then
   echo "Running migrations for $WORKER_MODE"
   yarn run predeploy
+
+  # Seed Colombia geo data if SEED_COLOMBIA_GEO is set
+  if [ "$SEED_COLOMBIA_GEO" = "true" ]; then
+    echo "Seeding Colombia geo data..."
+    yarn medusa exec ./src/scripts/seed-colombia-geo.js
+  fi
 else
   echo "Skipping migrations because WORKER_MODE=worker"
 fi

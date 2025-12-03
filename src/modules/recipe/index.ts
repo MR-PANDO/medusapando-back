66210b71a2cc88a9faa2726d87556f05c3b68830
MR-PANDO <gerencia@tecnoclinica.com>
2025-12-03 02:07:17 -0500

    recipe module migration working

--- src/modules/recipe/index.ts
+++ src/modules/recipe/index.ts
@@ -1,7 +1,8 @@
 import { Module } from "@medusajs/framework/utils"
 import RecipeModuleService from "./service"
 
-export const RECIPE_MODULE = "recipeModuleService"
+// Change this to just "recipe" if you want cleaner naming
+export const RECIPE_MODULE = "recipe"  
 
 export default Module(RECIPE_MODULE, {
   service: RecipeModuleService,

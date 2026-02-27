8141472cfcadccce36f05f8b61be9a289897b436
MR-PANDO <gerencia@tecnoclinica.com>
2026-02-26 23:36:15 -0500

    abandoned car and buy back integration

--- src/modules/smtp-notification/index.ts
+++ src/modules/smtp-notification/index.ts
@@ -1,8 +1,6 @@
-import { Module } from "@medusajs/framework/utils"
+import { ModuleProvider, Modules } from "@medusajs/framework/utils"
 import SmtpNotificationService from "./service"
 
-export const SMTP_NOTIFICATION_MODULE = "notification-smtp"
-
-export default Module(SMTP_NOTIFICATION_MODULE, {
-  service: SmtpNotificationService,
+export default ModuleProvider(Modules.NOTIFICATION, {
+  services: [SmtpNotificationService],
 })

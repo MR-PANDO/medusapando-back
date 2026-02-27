6dbd337a8867c206e9cea314c7e4b0aaf2ce0dd0
MR-PANDO <gerencia@tecnoclinica.com>
2026-02-27 13:13:27 -0500

    Seo integration

--- src/api/store/seo/[resource_type]/[resource_id]/route.ts
+++ src/api/store/seo/[resource_type]/[resource_id]/route.ts
@@ -7,7 +7,7 @@ export const GET = async (req: MedusaRequest, res: MedusaResponse) => {
 
   const { resource_type, resource_id } = req.params
 
-  const [record] = await seoService.listSeoMetadatas(
+  const [record] = await seoService.listSeoMetadata(
     {
       resource_type,
       resource_id,

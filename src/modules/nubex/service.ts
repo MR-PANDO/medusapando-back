6792dfbf6429c6815eb2112d83c5e4c3b4363f8e
MR-PANDO <gerencia@tecnoclinica.com>
2026-03-13 13:57:38 -0500

    inventory sync

--- src/modules/nubex/service.ts
+++ src/modules/nubex/service.ts
@@ -62,7 +62,7 @@ class NubexModuleService extends MedusaService({
           INNER JOIN dbo.tbreferencias AS r ON p.codigo_ref = r.codigo
           LEFT OUTER JOIN dbo.tbdetdivision AS dd ON p.codigo_detdiv = dd.codigo
           LEFT OUTER JOIN dbo.tbdetsubdivicion AS ds ON p.codigo_detsubdiv = ds.codigo
-          LEFT OUTER JOIN dbo.tbprecios AS pr ON p.codigo = pr.codigo_prod AND pr.idlistaprecio = 1
+          LEFT OUTER JOIN dbo.tbprecios AS pr ON p.codigo = pr.codigo_prod
           LEFT OUTER JOIN dbo.vw_existencias_ultima AS e
             ON p.codigo = e.codproducto AND e.rn = 1 AND e.codsucursal = @sucursal
           WHERE (p.activo = 1) AND (r.activo = 1) AND (r.escombo = 0)
